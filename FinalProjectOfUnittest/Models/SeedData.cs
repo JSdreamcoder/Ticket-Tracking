9a6a5de9850340653170c6f5eee6eb086f04384f
Jaewon <assess81@gmail.com>
2022-05-12 15:27:42 -0500

    Add Some seed data

---
+++ FinalProjectOfUnittest/Models/SeedData.cs
@@ -0,0 +1,33 @@
+﻿using FinalProjectOfUnittest.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinalProjectOfUnittest.Models
+{
+    public class SeedData
+    {
+        public async static Task Initialize(IServiceProvider serviceProvider)
+        {
+            var context = new ApplicationDbContext(serviceProvider.GetRequiredService<DbContextOptions<ApplicationDbContext>>());
+            serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            var userManager = serviceProvider.GetRequiredService<UserManager<AppUser>>();
+            var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            if(!context.Roles.Any())
+            {
+                List<string> roles = new List<string>()
+                {
+                    "Administrator","ProjectManager","Developer","Submitter"
+                };
+
+                foreach (string role in roles)
+                {
+                    await roleManager.CreateAsync(new IdentityRole(role));
+
+                }
+            }
+
+
+
+        }
+    }
+}
