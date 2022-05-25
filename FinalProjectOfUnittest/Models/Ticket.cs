829f6099d49bf8f781a34b352484319a475636c1
Jaewon <assess81@gmail.com>
2022-05-25 00:52:48 -0500

    ticket design

--- FinalProjectOfUnittest/Models/Ticket.cs
+++ FinalProjectOfUnittest/Models/Ticket.cs
@@ -1,9 +1,11 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using FinalProjectOfUnittest.Controllers;
+using System.ComponentModel.DataAnnotations;
 
 namespace FinalProjectOfUnittest.Models
 {
     public class Ticket
     {
+        public DeadLineStrategy DeadlineStrategy { get; set; }
         public int Id { get; set; } 
         public string Title { get; set; }   
         public string Description { get; set; }
@@ -12,7 +14,7 @@ namespace FinalProjectOfUnittest.Models
         public DateTime? Updated { get; set; }
         public int ProjectId { get; set; }
         public Project? Project { get; set; }
-        public TicketTypes TicketType { get; set; }
+        public virtual TicketTypes TicketType { get; set; }
         
         public TicketPriorities TicketPriority{ get; set; }
         
@@ -35,6 +37,13 @@ namespace FinalProjectOfUnittest.Models
 
         }
 
+        public int ResponseDeadLine {get; set;} // for set assignmentuser deadline
+        public int BreachDeadLine { get; set;} // for set Complete Deadline
+
+        public string ErrorCodes { get; set; } // for bugreport
+        public string ErrorLogs { get; set; } // for bugreport
+
+
     }
 
     public enum TicketStatus
@@ -54,16 +63,17 @@ namespace FinalProjectOfUnittest.Models
     }
 
     public enum TicketTypes
-    { 
+    {
         TicketType,
         GeneralQuestion,  // Low
         BugReport,        // High
         Payment,          // Medium
         TechIssue,        // Medium
-        AccountIssue      // High
+        AccountIssue,      // High
+        ServiceRequest
     }
 
-    
+
     public enum SortbyList
     {
         [Display(Name = "Select Sort Way")]
@@ -82,6 +92,178 @@ namespace FinalProjectOfUnittest.Models
         [Display(Name = "Ticket Status")]
         TicketStatus
             
+    }
+
+    public class Bugreports : Ticket { }
+    public class ServiceRequest : Ticket { }
+    public class AccountRequest : Ticket { }
+    public class TechRequest : Ticket { }
+    public class GeneralQuestion : Ticket { }
+    public class PaymentRequest : Ticket { }
+
+    public class TicketFactory
+    {
+        public Ticket CreateTicket(int id, string title, string description, TicketTypes type)
+        {
+            Ticket newTicket;
+
+            if (type == TicketTypes.AccountIssue)
+            {
+                newTicket = new AccountRequest { Id = id, Title = title, Description = description, TicketType = type };
+            }
+            else if (type == TicketTypes.Payment)
+            {
+                newTicket = new PaymentRequest { Id = id, Title = title, Description = description, TicketType = type };
+            }
+            else if (type == TicketTypes.TechIssue)
+            {
+                newTicket = new TechRequest { Id = id, Title = title, Description = description, TicketType = type };
+            }
+            else if (type == TicketTypes.ServiceRequest)
+            {
+                newTicket = new ServiceRequest { Id = id, Title = title, Description = description, TicketType = type };
+            }
+            else if (type == TicketTypes.GeneralQuestion)
+            {
+                newTicket = new GeneralQuestion { Id = id, Title = title, Description = description, TicketType = type };
+            }
+            else
+            {
+                throw new Exception("Wrong Type");
+            }
+            return newTicket;
+        }
+
+        public Ticket CreateTicket(int id, string title, string description, TicketTypes type, string errorCode, string errorLog)
+        {
+            if (type == TicketTypes.BugReport)
+            {
+                return new Bugreports { Id = id, Title = title, Description = description, TicketType = type, ErrorCodes = errorCode, ErrorLogs = errorLog };
+            }
+            else
+            {
+                throw new Exception("Wrong Type");
+            }
+        }
+    }
+
+    // Factory patterns
+    public class TicketSystem
+    {
+        public DeadLineStrategy DeadLineStrategy { get; set; }
+        public TicketFactory TicketFactory { get; set; }
+        public TicketSystem()
+        {
+            TicketFactory = new TicketFactory();
+        }
+        //create Ticket( 1.create object 2.setpriority by type 2.set deadline by priority 
+        // create tickets 
+        public Ticket CreateTicket(int id, string title, string description, TicketTypes type)
+        {
+            var newTicket = TicketFactory.CreateTicket(id, title, description, type);
+            newTicket = SetTicketPriority(newTicket);
+            newTicket = SetDeadLines(newTicket);
+
+            return newTicket;
+        }
+        // create tickets with errorCode and error logs
+        public Ticket CreateTicket(int id, string title, string description, TicketTypes type, string errorCode, string errorLog)
+        {
+            Ticket newTicket = TicketFactory.CreateTicket(id, title, description, type, errorCode, errorLog);
+            newTicket = SetTicketPriority(newTicket);
+            newTicket = SetDeadLines(newTicket);
+
+            return newTicket;
+        }
+
+        public Ticket SetTicketPriority(Ticket ticket)
+        {
+            //BugReport,        // High
+            //AccountIssue      // High
+            //Payment,          // Medium
+            //TechIssue,        // Medium
+            //ServiceRequest    // Medium
+            //GeneralQuestion,  // Low 
+            switch (ticket.TicketType)
+            {
+                case TicketTypes.BugReport:
+                case TicketTypes.AccountIssue:
+                    ticket.TicketPriority = TicketPriorities.High;
+                    break;
+
+                case TicketTypes.Payment:
+                case TicketTypes.TechIssue:
+                    ticket.TicketPriority = TicketPriorities.Medium;
+                    break;
+
+                case TicketTypes.ServiceRequest:
+                case TicketTypes.GeneralQuestion:
+                    ticket.TicketPriority = TicketPriorities.Medium;
+                    break;
+            }
+
+            return ticket;
+        }
+        public Ticket SetDeadLines(Ticket newticket)
+        {
+
+            // strategy selected by priority
+            if (newticket.TicketPriority == TicketPriorities.Low)// low
+            {
+                DeadLineStrategy = new LongDeadLine();
+
+            }
+            else if (newticket.TicketPriority == TicketPriorities.Medium)//medium
+            {
+                DeadLineStrategy = new MediumDeadLine();
+            }
+            else //high
+            {
+                DeadLineStrategy = new ShortDeadLine();
+            }
+
+            // run setDeadline
+            DeadLineStrategy.SetDeadLine(newticket);
+
+            // return
+            return newticket;
+        }
+
+
+    }
+
+
+    //  Strategy patterns
+    public interface DeadLineStrategy
+    {
+        public void SetDeadLine(Ticket ticket);
+    }
+
+    public class LongDeadLine : DeadLineStrategy
+    {
+        public void SetDeadLine(Ticket ticket)
+        {
+            ticket.ResponseDeadLine = 72;
+            ticket.BreachDeadLine = 240;
+        }
+    }
+    public class MediumDeadLine : DeadLineStrategy
+    {
+        public void SetDeadLine(Ticket ticket)
+        {
+            ticket.ResponseDeadLine = 48;
+            ticket.BreachDeadLine = 120;
+        }
+
+    }
+    public class ShortDeadLine : DeadLineStrategy
+    {
+        public void SetDeadLine(Ticket ticket)
+        {
+            ticket.ResponseDeadLine = 24;
+            ticket.BreachDeadLine = 72;
+        }
+
     }
 
 
