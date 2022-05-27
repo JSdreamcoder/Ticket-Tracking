d16e2a2eb7658e89ee20d6fea9ef6a782be06a0f
Jaewon <assess81@gmail.com>
2022-05-27 00:50:42 -0500

    assign submitter role to new user

--- FinalProjectOfUnittest/Controllers/HomeController.cs
+++ FinalProjectOfUnittest/Controllers/HomeController.cs
@@ -29,14 +29,21 @@ namespace FinalProjectOfUnittest.Controllers
             userManager = um;
         }   
       
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
             var userName = User.Identity.Name;
             if (userName != null)
             {
-               var userId = appUserBLL.Get(u=>u.UserName == userName).Id;
-               var noticesByUser = noticebll.GetList(n=>n.UserId==userId && n.Ticket.TicketStatus != TicketStatus.Completed);
-               ViewBag.NumOfNotices = noticesByUser.Count;
+                var user = appUserBLL.Get(u => u.UserName == userName);
+                var userId = user.Id;
+                var roles = await userManager.GetRolesAsync(user);
+                // Assign submitter role to new user 
+                if (!roles.Any())
+                {
+                    await userManager.AddToRoleAsync(user, "Submitter");
+                }
+                var noticesByUser = noticebll.GetList(n => n.UserId == userId && n.Ticket.TicketStatus != TicketStatus.Completed);
+                ViewBag.NumOfNotices = noticesByUser.Count;
 
             }
             
